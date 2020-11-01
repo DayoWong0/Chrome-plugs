c1af8f90dbb31612a96b3e009ddf8ffc56718696
Dayo <23241655+DayoWong0@users.noreply.github.com>
2020-11-01 21:25:13 +0800

    Update encoreTVB.js

--- tampermonkey/encoreTVB/encoreTVB.js
+++ tampermonkey/encoreTVB/encoreTVB.js
@@ -2,7 +2,7 @@
 // @name         encoreTVB 自动播放
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  encoreTVB 自动播放
+// @description  encoreTVB 自动播放插件
 // @author       https://github.com/DayoWong0/script
 // @match        https://www.encoretvb.com/*
 // @require     https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.js
