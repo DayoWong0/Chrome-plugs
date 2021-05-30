28d7cca701641b2b5bad485af37fbb0e089dc1ca
Dayo <23241655+DayoWong0@users.noreply.github.com>
2021-05-30 19:05:24 +0800

    Update site_title_and_link_to_markdown.js

--- tampermonkey/markdown/site_title_and_link_to_markdown.js
+++ tampermonkey/markdown/site_title_and_link_to_markdown.js
@@ -1,72 +1,54 @@
 // ==UserScript==
-// @name         引用网址和标题为 Markdown 链接
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  点击网页右下角蓝色的 md 按钮将当前的 标题 + 网址 复制为 Markdown 格式的链接，方便在 Markdown 格式的笔记中引用网站上的资料。例如 [百度一下，你就知道](https://www.baidu.com)
-// @author       https://github.com/DayoWong0/script
-// @include *
-// @require     https://cdn.bootcdn.net/ajax/libs/jquery/3.5.1/jquery.js
-// @grant        none
-// ==/UserScript==
+// @name            引用网页标题和网址为Markdown链接
+// @namespace       http://tampermonkey.net/
+// @description     Context menu to execute UserScript
+// @version         0.2
+// @author          https://dayowong.com/DayoWong0
+// @include         *
+// @run-at          context-menu
+// @require      https://cdn.jsdelivr.net/npm/clipboard@2.0.8/dist/clipboard.min.js
+// @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
 
-(function () {
-    'use strict'
-    // 新增一个 button
-    var but = document.createElement('button')
-    // 设置元素 id
-    but.id = "referSiteToMarkdownFormatLink"
-    but.innerHTML = "md"
-    but.addEventListener("click", referSiteToMarkdownFormatLink)
-    //but.addEventListener("mouseover",displayToBlock)
-    //but.addEventListener("mouseout",displayToNone)
-    // $("#referSiteToMarkdownFormatLink").
-    var parent = document.getElementsByTagName("body")
-    parent[0].appendChild(but)
-    var element = document.getElementById("referSiteToMarkdownFormatLink")
-    // css 参考来自 bilibili 首页悬浮固定的 联系客服
-    //element.style.cssText = 'position:fixed;z-index:9999;left:0;top:50%;margin-top:-36px;width:28px;height:200px;border-top-right-radius:2px;border-bottom-right-radius:2px;transition:all .3s;font-size:12px;color:#fff;box-shadow:0 6px 10px 0 rgba(251,114,153,.4);padding:8px 7px;line-height:14px}.try-feedback:hover{background-color:#ff85ad;color:#fff}.contact-help{position:fixed;z-index:101;left:0;top:calc(50% - 30px);margin-top:-36px;width:28px;height:72px;transition:all .3s;font-size:12px;color: red;background:#fff;border:1px solid #e7e7e7;box-shadow:0 6px 10px 0 #e7e7e7;border-radius:0 2px 2px 0;padding:8px 7px;line-height:14px';
-    //element.style.cssText = 'position: fixed;z-index: 9999;left: 0px;top: 50%;margin-top: -36px;width: 28px;height: 200px;border-top-right-radius: 2px;border-bottom-right-radius: 2px;transition: all 0.3s ease 0s;font-size: 12px;box-shadow: rgba(251, 114, 153, 0.4) 0px 6px 10px 0px;padding: 8px 7px;';
-    //var cssText = 'position: fixed;z-index: 9999; left: 0px;  bottom: 0%; /* margin top: -36px; */width: 27px;height: auto;border-top-right-radius: 2px;border-bottom-right-radius: 2px;transition: all 0.3s ease 0s;font-size: 1px;box-shadow: rgba(251, 114, 153, 0.4) 0px 6px 10px 0px;'
-    //var cssText = 'position: fixed;z-index: 9999;left: 0px;bottom: 50%;width: 27px;height: 17px;border-top-right-radius: 2px;-bottom-right-radius: 2px;transition: all 0.3s ease 0s;font-size: 1px;box-shadow: rgba(251, 114, 153, 0.4) 0px 6px 10px 0px;'
-    var cssText = 'position: fixed;z-index: 9999;left: 0px;bottom: 1%;/* width: 27px; *//* height: 17px; */ /* border-top-right-radius: 2px; */ transition: all 0.3s ease 0s;font-size: 1px;box-shadow: rgba(251, 114, 153, 0.4) 0px 6px 10px 0px;color: blue'
-    element.style.cssText = cssText
-    // console.log("是否全屏：" + isFullScreen())
+// ==/UserScript==]
 
-    function displayToBlock () {
-        var element = document.getElementById("referSiteToMarkdownFormatLink")
-        console.log("on mouse over")
-        element.style.display = 'block'
-    }
-
-    function displayToNone () {
-        var element = document.getElementById("referSiteToMarkdownFormatLink")
-        console.log("on mouse out")
-        // element.style.display = 'none'
-    }
-
-    function referSiteToMarkdownFormatLink () {
-        let link = window.location.href
-        let title = document.title
-
-        // 复制到剪切板
-        var copyText = '[' + title + '](' + link + ')'
-        var el = document.createElement('textarea')
-        el.value = copyText
-        el.setAttribute('readonly', '')
-        el.style = {
-            position: 'absolute',
-            left: '-9999px'
-        }
-        document.body.appendChild(el)
-        el.select()
-        document.execCommand('copy')
-        document.body.removeChild(el)
-        //  阻止事件冒泡
-        event.stopPropagation()
-    }
-
-    //判断浏览器是否全屏
-    function isFullScreen () {
-        return (document.body.scrollHeight == window.screen.height && document.body.scrollWidth == window.screen.width)
-    }
-})()
+(function() {
+    'use strict';
+    //console.log("复制为MarkDown链接被调用了")
+    //console.log(document.title)
+    //console.log(location.href)
+    let link = window.location.href
+    let title = document.title
+    // markdown格式的链接
+    let copyText = "[" + title + "](" + link + ")"
+    //console.log(copyText)
+    //创建一个不可见的button，用于模拟触发Click事件并复制
+    let createDiv=document.createElement("div")
+    createDiv.innerHTML='<button id="copyAsMdLink" style="display: none"></button>'
+    document.body.appendChild(createDiv)
+    //new clipboard
+    let clipboard = new ClipboardJS('#copyAsMdLink', {
+    text: function(trigger) {
+        return copyText
+    }});
+    clipboard.on('success', function(e) {
+        console.info("复制成功");
+        e.clearSelection();
+        Swal.fire({
+            icon: 'success',
+            title: '成功!',
+            text: '已引用为Markdown格式链接',
+            timer: 2000
+        })
+    });
+    clipboard.on('error', function(e) {
+        console.info("复制失败");
+                Swal.fire({
+            icon: 'error',
+            title: '失败!',
+            text: '复制失败',
+            timer: 2000
+        })
+    });
+    // 模拟点击事件
+    document.getElementById("copyAsMdLink").click()
+})();
