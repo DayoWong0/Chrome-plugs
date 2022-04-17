2d668c8a4737729ad57452431f52f39f4b9d76c5
Dayo <1690328478@qq.com>
2022-04-17 11:35:39 +0800

    Update searchBookInZLibrary.js

--- tampermonkey/searchBookInZLibrary.js
+++ tampermonkey/searchBookInZLibrary.js
@@ -1,24 +1,25 @@
 // ==UserScript==
-// @name         在z-library中搜索豆瓣书籍
+// @name         在z-library中搜索
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  在z-library中搜索豆瓣的书籍
+// @version      0.2
+// @description  在z-library中搜索选中的文字
 // @author       You
 // @match        https://book.douban.com/subject/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=douban.com
 // @grant          GM_registerMenuCommand
 // @license MIT
 // ==/UserScript==
- 
+
 (function() {
     'use strict';
     function searchBookInZlibrary(){
-    let book_name = document.getElementsByTagName('h1')[0].innerText;
+    //let book_name = document.getElementsByTagName('h1')[0].innerText;
+    let book_name = window.getSelection().toString()
     let url = "https://zh.book4you.org/s/"+book_name;
     window.open(url,'target','');
     }
- 
-    GM_registerMenuCommand("在z-library中搜索书籍", () => {
+
+    GM_registerMenuCommand("在z-library中搜索", () => {
         searchBookInZlibrary()
     });
 })();
