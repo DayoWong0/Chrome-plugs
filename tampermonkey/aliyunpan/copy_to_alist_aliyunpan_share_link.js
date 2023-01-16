0d9bb2ee3c8eaf816e9d136d36f8da0824f1eb08
Dayo <1690328478@qq.com>
2023-01-16 23:06:51 +0800

    Update copy_to_alist_aliyunpan_share_link.js

--- tampermonkey/aliyunpan/copy_to_alist_aliyunpan_share_link.js
+++ tampermonkey/aliyunpan/copy_to_alist_aliyunpan_share_link.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name            复制为添加到alist阿里云盘分享链接的格式
-// @namespace       http://tampermonkey.net/
+// @namespace       https://github.com/DayoWong0/script
 // @description     配合该项目使用 https://github.com/yzbtdiy/alist_batch_add
-// @version         0.1
-// @author       https://dayowong.com/DayoWong0
+// @version         0.2
+// @author       https://github.com/DayoWong0/script
 // @match        https://www.aliyundrive.com/s/*
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2@11
 // @grant        GM_registerMenuCommand
@@ -20,22 +20,20 @@
                 timer: 2000
             })
     }
-    GM_registerMenuCommand("复制文件目录名及链接", () =>
+        GM_registerMenuCommand("复制2级文件目录名及链接", () =>
                            {GM_setClipboard(`${document.getElementsByClassName('breadcrumb-item-link--M-p4b')[1].innerText}: ${document.URL}`)
                            successAlert()
                            }
                           );
-    GM_registerMenuCommand("仅复制标题", () => {GM_setClipboard(document.getElementsByClassName('breadcrumb-item-link--M-p4b')[1].innerText)
-                                           successAlert()
-                                          }
-                                           );
-    GM_registerMenuCommand("仅复制链接", () => {GM_setClipboard(document.URL)
-                                          successAlert()
-                                          });
-    document.onkeydown = function(event) {
-        if (event.altKey && event.keyCode == 66) {
-            GM_setClipboard(`[${document.title}](${document.URL})`);
-            successAlert()
-        }
-    }
+        GM_registerMenuCommand("复制3级文件目录名及链接", () =>
+                           {GM_setClipboard(`${document.getElementsByClassName('breadcrumb-item-link--M-p4b')[2].innerText}: ${document.URL}`)
+                           successAlert()
+                           }
+                          );
+            GM_registerMenuCommand("复制4级文件目录名及链接", () =>
+                           {GM_setClipboard(`${document.getElementsByClassName('breadcrumb-item-link--M-p4b')[3].innerText}: ${document.URL}`)
+                           successAlert()
+                           }
+                          );
+
 })();
