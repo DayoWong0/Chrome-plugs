54abd74acda3141360854e60cfa7ba1a9c6f8aed
Dayo <1690328478@qq.com>
2023-01-19 22:24:55 +0800

    Update QQ_Link_add_tag_a.js

--- tampermonkey/QQ/QQ_Link_add_tag_a.js
+++ tampermonkey/QQ/QQ_Link_add_tag_a.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         QQ链接直接访问
-// @namespace    https://github.com/DayoWong0/script/new/master/tampermonkey/QQ/QQ_Link_add_tag_a.js
+// @namespace    https://github.com/DayoWong0/script/edit/master/tampermonkey/QQ/QQ_Link_add_tag_a.js
 // @version      0.1
 // @description  电脑QQ访问链接时跳转页面加了个a标签，不用复制网址直接点击即可访问网址
 // @author       DayoWong0
